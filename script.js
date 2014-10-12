8bf025969d621c6d625576c67a6785a059168d34
Aaron Schumacher <ajschumacher@gmail.com>
2014-10-11 22:38:23 -0400

    organize highlighting differently

--- script.js
+++ script.js
@@ -2,5 +2,3 @@ var pathName = window.location.pathname;
 var dirName = pathName.substring(0, pathName.lastIndexOf('/')+1);
 var editLink = document.getElementById('edit');
 editLink.setAttribute('href', 'https://github.com/ajschumacher/ajschumacher.github.io/tree/master' + dirName)
-
-hljs.initHighlightingOnLoad();
