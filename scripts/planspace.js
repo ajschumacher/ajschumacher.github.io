33b90645d00fb661709b11378907d2bec2ea9514
Aaron Schumacher <ajschumacher@gmail.com>
2014-10-15 17:50:22 +0000

    remove back link on main page; closes #29

--- scripts/planspace.js
+++ scripts/planspace.js
@@ -1,4 +1,8 @@
 var pathName = window.location.pathname;
 var dirName = pathName.substring(0, pathName.lastIndexOf('/')+1);
-var editLink = document.getElementById('edit');
-editLink.setAttribute('href','https://github.com/ajschumacher/ajschumacher.github.io/tree/master' + dirName);
+var edit_link = document.getElementById('edit_link');
+edit_link.setAttribute('href','https://github.com/ajschumacher/ajschumacher.github.io/tree/master' + dirName);
+if (pathName === "/") {
+    back_link = document.getElementById('back_link')
+    back_link.parentNode.removeChild(back_link)
+}
