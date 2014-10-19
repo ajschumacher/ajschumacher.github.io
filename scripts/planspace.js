256c2419bdc8360ae9b052410c90c6bd134459fd
Aaron Schumacher <ajschumacher@gmail.com>
2014-10-19 17:28:05 -0400

    specific edit link; closes #40
    (Based on how http://adv-r.had.co.nz/ does it.)

--- scripts/planspace.js
+++ scripts/planspace.js
@@ -1,7 +1,7 @@
 var pathName = window.location.pathname;
 var dirName = pathName.substring(0, pathName.lastIndexOf('/')+1);
 var edit_link = document.getElementById('edit_link');
-edit_link.setAttribute('href','https://github.com/ajschumacher/ajschumacher.github.io/tree/master' + dirName);
+edit_link.setAttribute('href','https://github.com/ajschumacher/ajschumacher.github.io/edit/master' + dirName + 'index.md');
 if (pathName === "/") {
     back_link = document.getElementById('back_link')
     back_link.parentNode.removeChild(back_link)
