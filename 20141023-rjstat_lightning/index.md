bcc546a7dcebf93e2e5b8e3ddb03815420d27111
Aaron Schumacher <ajschumacher@gmail.com>
2014-10-19 20:48:42 -0400

    initial sliding; tear out to raw big; closes #42

---
+++ 20141023-rjstat_lightning/index.md
@@ -0,0 +1,17 @@
+# JSON-stat and `rjstat`
+
+This is a lightning talk.
+
+-----
+
+## Slide contents!
+
+-----
+
+More text
+
+-----
+
+Another slide...
+
+-----
--- make_page.py
+++ make_page.py
@@ -26,6 +26,25 @@ def file_or_bust(dir, filename):
         return file_or_bust(parent_dir, filename)
 
 
+def slides_from(lines):
+    in_slide = False
+    slides = []
+    for line in lines:
+        if line.strip() == "-----":
+            if not in_slide:
+                in_slide = True
+                slide = []
+            else:
+                in_slide = False
+                slide = ''.join(slide)
+                slide = markdown.markdown(slide)
+                slide = '<div>\n' + slide + '\n</div>\n'
+                slides.append(slide)
+        elif in_slide:
+            slide.append(line)
+    return ''.join(slides)
+
+
 def add_code_blocks(lines):
     in_block = False
     for i, line in enumerate(lines):
@@ -60,17 +79,30 @@ def make_page(filename):
     header = markdown.markdown(title_line)
     title = title_to_text(title_line)
     lines = add_code_blocks(lines)
+    slides = slides_from(lines)
+    if slides:
+        slides_start = file_or_bust(calling_dir, 'slides_header.html')
+        slides_end = file_or_bust(calling_dir, 'slides_footer.html')
+        slides_html = slides_start + slides + slides_end
+    else:
+        slides_html = None
     body = markdown.markdown("".join(lines))
     start = file_or_bust(calling_dir, 'header.html')
     start = start.replace('HEAD_TITLE', title)
     end = file_or_bust(calling_dir, 'footer.html')
-    return start + header + body + end
+    plain_html = start + header + body + end
+    return plain_html, slides_html
 
 
 def main():
-    result = make_page('index.md')
+    plain_html, slides_html = make_page('index.md')
     with codecs.open('index.html', 'w', encoding='utf-8') as f:
-        f.write(result)
+        f.write(plain_html)
+    if slides_html:
+        if not os.path.exists('big/'):
+            os.makedirs('big/')
+        with codecs.open('big/index.html', 'w', encoding='utf-8') as f:
+            f.write(slides_html)
 
 if __name__ == '__main__':
     main()
